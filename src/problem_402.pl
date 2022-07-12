bdf9766a4ab81f49a7ae6af0c161d758575c2701
Vasily Kartashov <info@kartashov.com>
2022-07-12 13:07:41 +1000

    402

--- src/problem_402.pl
+++ src/problem_402.pl
@@ -8,15 +8,15 @@ balanced_tree(I, t(_, A, B)) :-
     I1 #= I - 1,
     R #= I1 mod 2,
     I2 #= (I1 - R) / 2,
-    balanced_tree_(I, t(_, A, B), I2, R).
-balanced_tree_(_, t(_, A, B), I, 0) :-
+    balanced_branches(I2, R, A, B).
+balanced_branches(I, 0, A, B) :-
     balanced_tree(I, A),
     balanced_tree(I, B).
-balanced_tree_(_, t(_, A, B), I, 1) :-
+balanced_branches(I, 1, A, B) :-
     I1 #= I + 1,
     balanced_tree(I, A),
     balanced_tree(I1, B).
-balanced_tree_(_, t(_, A, B), I, 1) :-
+balanced_branches(I, 1, A, B) :-
     I1 #= I + 1,
     balanced_tree(I1, A),
     balanced_tree(I, B).
