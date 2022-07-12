8e30d422b53f09655399bdf736c07f552cd997b3
Vasily Kartashov <info@kartashov.com>
2022-07-12 18:52:27 +1000

    120

---
+++ src/problem_120.pl
@@ -0,0 +1,9 @@
+:- module(problem_120, [list_at_removed/3]).
+
+:- use_module(library(clpz)).
+
+list_at_removed([_|T], 1, T).
+list_at_removed([A|T], I, [A|U]) :-
+    I #> 1,
+    I1 #= I - 1,
+    list_at_removed(T, I1, U).
