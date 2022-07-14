5cf0d2409a564784f588654c71cbe329c05af22f
vka <info@kartashov.com>
2022-07-14 20:26:35 +1000

    Update problem_110.pl

--- src/problem_110.pl
+++ src/problem_110.pl
@@ -5,7 +5,7 @@
 :- use_module(library(dif)).
 
 list_encoded([], []).
-list_encoded(A, [[N,C]|T]):-
+list_encoded(A, [[N,C]|T]) :-
     list_top_length_tail(A, C, N, U),
     list_encoded(U, T).
 
@@ -13,6 +13,6 @@ list_top_length_tail([A], A, 1, []).
 list_top_length_tail([A,B|C], A, 1, [B|C]) :-
     dif(A, B).
 list_top_length_tail([A,A|C], A, N, T) :-
-    N #> 1,
-    N #= N1 + 1,
+    #N #> 1,
+    #N #= N1 + 1,
     list_top_length_tail([A|C], A, N1, T).
