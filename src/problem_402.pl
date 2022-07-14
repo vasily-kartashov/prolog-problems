2a9b46ef801a2dec0f30c33e30905238cbd9d745
vka <info@kartashov.com>
2022-07-14 20:29:35 +1000

    Update problem_402.pl

--- src/problem_402.pl
+++ src/problem_402.pl
@@ -4,11 +4,12 @@
 
 balanced_tree(0, nil).
 balanced_tree(I, t(_, A, B)) :-
-    I #> 0,
+    #I #> 0,
     I1 #= I - 1,
     R #= I1 mod 2,
     I2 #= (I1 - R) / 2,
     balanced_branches(I2, R, A, B).
+    
 balanced_branches(I, 0, A, B) :-
     balanced_tree(I, A),
     balanced_tree(I, B).
