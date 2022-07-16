1792652fc0d6d8b81eacc11c3f3553efa5b411dc
Vasily Kartashov <info@kartashov.com>
2022-07-16 19:47:40 +1000

    204

---
+++ src/problem_204.pl
@@ -0,0 +1,19 @@
+:- module(problem_204, [range_primes/3]).
+
+:- use_module(problem_201).
+:- use_module(library(clpz)).
+
+range_primes(A, B, R) :-
+    #A #=< #B,
+    primes(Ps),
+    range_primes(A, B, R, Ps).
+
+range_primes(A, B, T, [P|U]) :-
+    P #< A,
+    range_primes(A, B, T, U).
+range_primes(A, B, [P|T], [P|Ps]) :-
+    A #=< P,
+    P #=< B,
+    range_primes(P, B, T, Ps).
+range_primes(_, B, [], [P|_]) :-
+    B #< P.
