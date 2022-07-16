731b15358cb6b1da915d40b221a6f98fda65ad32
Vasily Kartashov <info@kartashov.com>
2022-07-16 18:20:49 +1000

    104

--- src/problem_104.pl
+++ src/problem_104.pl
@@ -1,13 +1,13 @@
-:- module(problem_104, [list_length/2]).
+:- module(problem_104, [length/2]).
 
 :- use_module(library(clpz)).
 
-list_length(L, N) :-
+length(L, N) :-
     #N #>= 0,
-    list_length(L, 0, N).
+    length(L, 0, N).
 
-list_length([], N, N).
-list_length([_|T], I, N) :-
+length([], N, N).
+length([_|T], I, N) :-
     I #< N,
     I1 #= I + 1,
-    list_length(T, I1, N).
+    length(T, I1, N).
