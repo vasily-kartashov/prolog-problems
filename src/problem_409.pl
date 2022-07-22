4a391d456219c94f5880e0cd2473914dc6d59680
Vasily Kartashov <info@kartashov.com>
2022-07-22 18:03:55 +1000

    409

--- src/problem_409.pl
+++ src/problem_409.pl
@@ -3,14 +3,13 @@
 :- use_module(library(dif)).
 :- use_module(library(lists)).
 
+tree_leaves(nil, []).
 tree_leaves(t(A, nil, nil), [A]).
-tree_leaves(t(_, B, nil), L) :-
-    dif(B, nil),
-    tree_leaves(B, L).
-tree_leaves(t(_, nil, B), L) :-
-    dif(B, nil),
-    tree_leaves(B, L).
-tree_leaves(t(_, B, C), L) :-
-    append(L0, L1, L),
-    tree_leaves(B, L0),
-    tree_leaves(C, L1).
+tree_leaves(t(_, t(A, B, C), nil), [L|Ls]) :-
+    tree_leaves(t(A, B, C), [L|Ls]).
+tree_leaves(t(_, nil, t(A, B, C)), [L|Ls]) :-
+    tree_leaves(t(A, B, C), [L|Ls]).
+tree_leaves(t(_, t(A0, B0, C0), t(A1, B1, C1)), [L0,L1|Ls]) :-
+    tree_leaves(t(A0, B0, C0), [P0|Ps]),
+    tree_leaves(t(A1, B1, C1), [S0|Ss]),
+    append([P0|Ps], [S0|Ss], [L0,L1|Ls]).
