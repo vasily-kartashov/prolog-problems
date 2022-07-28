ade7c40b3b6e18e12d71691c707ac3bba84a707d
Vasily Kartashov <info@kartashov.com>
2022-07-28 12:09:44 +1000

    506

--- src/problem_506.pl
+++ src/problem_506.pl
@@ -5,9 +5,20 @@
 tree_ltl(T, L) :-
     phrase(tree_ltl(T), L).
 
-tree_ltl(t(A, [])) --> [A].
-tree_ltl(t(A, [B|T])) --> "(", [A], " ", children_ltl([B|T]), ")".
+tree_ltl(t(A, [])) -->
+    [A].
+tree_ltl(t(A, [B|T])) -->
+    "(",
+    [A],
+    " ",
+    children_ltl([B|T]),
+    ")".
 
-children_ltl([]) --> "".
-children_ltl([A]) --> tree_ltl(A).
-children_ltl([B,C|T]) --> tree_ltl(B), " ", children_ltl([C|T]).
+children_ltl([]) -->
+    "".
+children_ltl([A]) -->
+    tree_ltl(A).
+children_ltl([B,C|T]) -->
+    tree_ltl(B),
+    " ",
+    children_ltl([C|T]).
