d57b125857ffc98282c5396f1f695d58dce480c7
Vasily Kartashov <info@kartashov.com>
2022-07-29 23:27:48 +1000

    plist

--- src/plist.pl
+++ src/plist.pl
@@ -8,11 +8,14 @@ is_plist([I-_,J-B|T]) :-
     I #>= J,
     is_plist([J-B|T]).
 
-plist([], A, [A]).
-plist([I-A|T], J-B, [J-B,I-A|T]) :-
-    J #>= I,
-    is_plist([I-A|T]).
-plist([I-A|T], J-B, [I-A|C]) :-
+plist(A, I-B, C) :-
+    plist1(A, I-B, C),
+    is_plist(A),
+    is_plist(C).
+
+plist1([], A, [A]).
+plist1([I-A|T], J-B, [J-B,I-A|T]) :-
+    J #>= I.
+plist1([I-A|T], J-B, [I-A|C]) :-
     J #=< I,
-    plist(T, J-B, C),
-    is_plist([I-A|T]).
+    plist1(T, J-B, C).
