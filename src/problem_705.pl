51acaecac14a1ad5f79d49f6f5453cb6d4b90542
Vasily Kartashov <info@kartashov.com>
2022-07-30 21:55:55 +1000

    75

---
+++ src/problem_705.pl
@@ -0,0 +1,33 @@
+:- module(problem_705, [number_words/2]).
+
+:- use_module(library(clpz)).
+:- use_module(library(dcgs)).
+
+number_words(N, S) :-
+    phrase(number(N, S, _), S).
+
+digit(0) --> "zero".
+digit(1) --> "one".
+digit(2) --> "two".
+digit(3) --> "three".
+digit(4) --> "four".
+digit(5) --> "five".
+digit(6) --> "six".
+digit(7) --> "seven".
+digit(8) --> "eight".
+digit(9) --> "nine".
+
+number(I, L, L) -->
+    { I #< 10 },
+    digit(I).
+
+number(I, [_,_,_|L0], L) -->
+    { I #> 9,
+      R #= I mod 10,
+      J #= I // 10
+    },
+    number(J, L0, L),
+    ['-'],
+    digit(R).
+
+
