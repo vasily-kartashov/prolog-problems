9b032486f38a62adb3a95011f14cb03b6e18a2c5
Vasily Kartashov <info@kartashov.com>
2022-08-05 15:30:21 +1000

    602:

---
+++ src/problem_602.pl
@@ -0,0 +1,13 @@
+:- module(problem_602, [path/5]).
+
+:- use_module(library(lists)).
+
+path(graph(Nodes, Edges), A, B, P, D) :-
+    path(graph(Nodes, Edges), A, B, [], P, D).
+
+path(graph(_, _), A, A, _, [A], 0).
+path(graph(Nodes, Edges), A, B, V, [A|T], D) :-
+    member(a(A, C, D0), Edges),
+    \+ member(C, V),
+    path(graph(Nodes, Edges), C, B, [A|V], T, D1),
+    D is D0 + D1.
