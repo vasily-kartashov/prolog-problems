6b0cdf35fd0d3078130ebd8f852ac0e2de8b6153
Vasily Kartashov <info@kartashov.com>
2022-08-09 19:20:11 +1000

    604

--- src/problem_604.pl
+++ src/problem_604.pl
@@ -1,18 +1,18 @@
-:- module(problem_604, [s_tree/2]).
+:- module(problem_604, [s_tree/3]).
 
 :- use_module(library(lists)).
 :- use_module(library(ordsets)).
 
-s_tree(graph(Nodes, Edges), Tree) :-
+s_tree(graph(Nodes, Edges), Tree, Sum) :-
     member(A, Nodes),
     ord_del_element(Nodes, A, Nodes0),
-    s_tree(A, Nodes0, [], Edges, Tree).
+    s_tree(A, Nodes0, [], Edges, Tree, Sum).
 
-s_tree(A, Nodes, Nodes, _, t(A, [])).
-s_tree(A, Nodes, Nodes2, Edges, t(A, [T|Children])) :-
+s_tree(A, Nodes, Nodes, _, t(A, []), 0).
+s_tree(A, Nodes, Nodes2, Edges, t(A, [T|Children]), Sum) :-
     member(B, Nodes),
-    member(a(A, B, _), Edges),
+    member(a(A, B, W), Edges),
     ord_del_element(Nodes, B, Nodes0),
-    s_tree(B, Nodes0, Nodes1, Edges, T),
-    s_tree(A, Nodes1, Nodes2, Edges, t(A, Children)).
-
+    s_tree(B, Nodes0, Nodes1, Edges, T, Sum1),
+    s_tree(A, Nodes1, Nodes2, Edges, t(A, Children), Sum2),
+    Sum is Sum1 + Sum2 + W.
