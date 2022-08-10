f6c3faf80bc67bacdc5f926729df00a13625c800
Vasily Kartashov <info@kartashov.com>
2022-08-10 17:16:22 +1000

    604

--- src/problem_604.pl
+++ src/problem_604.pl
@@ -1,18 +1,15 @@
 :- module(problem_604, [s_tree/3]).
 
 :- use_module(library(lists)).
-:- use_module(library(ordsets)).
 
 s_tree(graph(Nodes, Edges), Tree, Sum) :-
-    member(A, Nodes),
-    ord_del_element(Nodes, A, Nodes0),
+    select(A, Nodes, Nodes0),
     s_tree(A, Nodes0, [], Edges, Tree, 0, Sum).
 
 s_tree(A, Nodes, Nodes, _, t(A, []), Sum, Sum).
 s_tree(A, Nodes, Nodes2, Edges, t(A, [T|Children]), Sum0, Sum) :-
-    member(B, Nodes),
+    select(B, Nodes, Nodes0),
     member(a(A, B, W), Edges),
-    ord_del_element(Nodes, B, Nodes0),
     Sum1 is Sum0 + W,
     s_tree(B, Nodes0, Nodes1, Edges, T, Sum1, Sum2),
     s_tree(A, Nodes1, Nodes2, Edges, t(A, Children), Sum2, Sum).
