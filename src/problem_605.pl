cda7a12ebd4ad0e69f79a1251d8ac038be04e308
Vasily Kartashov <info@kartashov.com>
2022-08-10 17:12:46 +1000

    605

--- src/problem_605.pl
+++ src/problem_605.pl
@@ -4,20 +4,17 @@
 :- use_module(library(dif)).
 :- use_module(library(lambda)).
 :- use_module(library(lists)).
-:- use_module(library(ordsets)).
 
 ms_tree(graph(Nodes, Edges), Tree, Sum) :-
     foldl(\a(A, B, D) ^ L0 ^ plist(L0, D-(A-B)), Edges, [], Edges1),
-    member(A, Nodes),
-    ord_del_element(Nodes, A, Nodes1),
+    select(A, Nodes, Nodes1),
     ms_tree(Edges1, [A], Nodes1, t(A, []), Tree, 0, Sum).
 
 ms_tree(_, _, [], Tree, Tree, Sum, Sum).
 ms_tree(Edges, Visited, Outstanding, Tree0, Tree, Sum0, Sum) :-
     plist(Edges1, D-(A-B), Edges),
     member(A, Visited),
-    member(B, Outstanding),
-    ord_del_element(Outstanding, B, Outstanding1),
+    select(B, Outstanding, Outstanding1),
     tree_add(Tree0, A-B, Tree1),
     Sum1 is Sum0 + D,
     ms_tree(Edges1, [B|Visited], Outstanding1, Tree1, Tree, Sum1, Sum).
