31bef9e05fe2edeaf591fb48a222bb7edc6e6731
Vasily Kartashov <info@kartashov.com>
2022-08-10 17:10:27 +1000

    608

--- src/problem_608.pl
+++ src/problem_608.pl
@@ -1,16 +1,13 @@
 :- module(problem_608, [traverse/3]).
 
 :- use_module(library(lists)).
-:- use_module(library(ordsets)).
 
 traverse(graph(Nodes, Edges), A, [A|T]) :-
-    member(A, Nodes),
-    ord_del_element(Nodes, A, Nodes1),
+    select(A, Nodes, Nodes1),
     dfa(Edges, A, Nodes1, T).
 
 dfa(_, _, [], []).
 dfa(Edges, A, Outstanding, [B|Tail]) :-
     member(a(A, B, _), Edges),
-    member(B, Outstanding),
-    ord_del_element(Outstanding, B, Outstanding1),
+    select(B, Outstanding, Outstanding1),
     dfa(Edges, B, Outstanding1, Tail).
