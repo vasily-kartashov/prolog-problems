fa40ab756184ff55e58e123257621777f258d277
Vasily Kartashov <info@kartashov.com>
2022-08-11 11:14:59 +1000

    111

---
+++ src/problem_111.pl
@@ -0,0 +1,18 @@
+:- module(problem_111, [encode_modified/2]).
+
+:- use_module(library(clpz)).
+:- use_module(library(dif)).
+
+encode_modified([], []).
+encode_modified([A], [A]).
+encode_modified([A,B|T], [A|E]) :-
+    dif(A, B),
+    encode_modified([B|T], E).
+encode_modified([A,A|T], [[A,N]|E]) :-
+    N1 #= N - 1,
+    N1 #> 1,
+    encode_modified([A|T], [[A,N1]|E]).
+encode_modified([A,A|T], [[A,N]|E]) :-
+    N1 #= N - 1,
+    N1 #= 1,
+    encode_modified([A|T], [A|E]).
