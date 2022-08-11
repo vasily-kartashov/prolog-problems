e6f6f241b600af10465744fafe7245301a5e357a
Vasily Kartashov <info@kartashov.com>
2022-08-11 11:48:33 +1000

    115

--- src/problem_115.pl
+++ src/problem_115.pl
@@ -1,18 +1,18 @@
-:- module(problem_115, [list_n_duplicated/3]).
+:- module(problem_115, [dupli/3]).
 
 :- use_module(library(clpz)).
 
-list_n_duplicated(_, 0, []).
-list_n_duplicated(A, I, B) :-
-    #I #> 0,
-    list_n_i_duplicated(A, I, 0, B).
-
-list_n_i_duplicated([], _, _, []).
-list_n_i_duplicated([_|T], I, I, R) :-
+dupli(_, 0, []).
+dupli(A, I, B) :-
     I #> 0,
-    list_n_i_duplicated(T, I, 0, R).
-list_n_i_duplicated([A|T], I, J, [A|R]) :-
+    dupli(A, I, 0, B).
+
+dupli([], _, _, []).
+dupli([_|T], I, I, R) :-
+    I #> 0,
+    dupli(T, I, 0, R).
+dupli([A|T], I, J, [A|R]) :-
     J #>= 0,
     I #> J,
     J1 #= J + 1,
-    list_n_i_duplicated([A|T], I, J1, R).
+    dupli([A|T], I, J1, R).
