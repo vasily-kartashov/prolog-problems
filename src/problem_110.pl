f9a391d51c6c58bd9465e16b712ca2b2b59a741e
Vasily Kartashov <info@kartashov.com>
2022-08-11 10:58:41 +1000

    110

--- src/problem_110.pl
+++ src/problem_110.pl
@@ -1,18 +1,13 @@
-:- module(problem_110, [list_encoded/2]).
+:- module(problem_110, [encode/2]).
 
-:- use_module(library(lists)).
-:- use_module(library(clpz)).
 :- use_module(library(dif)).
 
-list_encoded([], []).
-list_encoded(A, [[N,C]|T]) :-
-    list_top_length_tail(A, C, N, U),
-    list_encoded(U, T).
-
-list_top_length_tail([A], A, 1, []).
-list_top_length_tail([A,B|C], A, 1, [B|C]) :-
-    dif(A, B).
-list_top_length_tail([A,A|C], A, N, T) :-
-    #N #> 1,
-    #N #= N1 + 1,
-    list_top_length_tail([A|C], A, N1, T).
+encode([], []).
+encode([A], [[A,1]]).
+encode([A,B|T], [[A,1]|E]) :-
+    dif(A, B),
+    encode([B|T], E).
+encode([A,A|T], [[A,N]|E]) :-
+    N #> 0,
+    N1 #= N - 1,
+    encode([A|T], [[A,N1]|E]).
