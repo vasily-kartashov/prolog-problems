093a8940e214a32e15f20f481382fd653918a69a
Vasily Kartashov <info@kartashov.com>
2022-08-12 20:38:42 +1000

    103

--- src/problem_103.pl
+++ src/problem_103.pl
@@ -1,9 +1,31 @@
-:- module(problem_103, [nth/3]).
+:- module(problem_103, [my_nth1/3]).
 
-:- use_module(library(clpz)).
+:- use_module(library(clpfd)).
 
-nth(1, [A|_], A).
-nth(I, [_|T], A) :- 
-    #I #> 1, 
+my_nth1(1, [A|_], A).
+my_nth1(I, [_|T], A) :- 
+    I #> 1,
     I1 #= I - 1, 
-    nth(I1, T, A).
+    my_nth1(I1, T, A).
+
+:- begin_tests(problem_103).
+
+test(my_nth1, [fail]) :-
+    my_nth1(1, [], _).
+test(my_nth1, [nondet]) :-
+    my_nth1(1, [A], A).
+test(my_nth1, [nondet]) :-
+    my_nth1(2, [_,A], A).
+test(my_nth1, [fail]) :-
+    my_nth1(0, [_], _).
+test(my_nth1, [fail]) :-
+    my_nth1(3, [_,_], _).
+test(my_nth1, [nondet]) :-
+    my_nth1(3, [_|T], A),
+    my_nth1(2, T, A).
+test(my_nth1, all(I-A == [1-a,2-b,3-c])) :-
+    my_nth1(I, [a,b,c], A).
+test(my_nth1, all(I == [1, 4])) :-
+    my_nth1(I, [a,b,c,a], a).
+
+:- end_tests(problem_103).
