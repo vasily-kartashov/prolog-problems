445f42420013494426a0a88947e0c3a7170c5bee
Vasily Kartashov <info@kartashov.com>
2022-08-12 20:19:40 +1000

    102

--- src/problem_102.pl
+++ src/problem_102.pl
@@ -1,17 +1,28 @@
-:- module(problem_102, [rnth/3]).
+:- module(problem_102, [rnth1/3]).
 
-:- use_module(library(clpz)).
+:- use_module(library(clpfd)).
 :- use_module(library(lists)).
 
-rnth(A, I, E) :-
-    #I #> 0,
+rnth1(I, A, E) :-
+    I #> 0,
     length(A, L),
-    J #= L - I + 1,
-    J #> 0,
-    at(A, J, E).
+    J #= L - I,
+    nth0(J, A, E).
 
-at([A|_], 1, A).
-at([_|T], I, A) :-
-    I #> 1,
-    I1 #= I - 1,
-    at(T, I1, A).
+:- begin_tests(problem_102).
+
+test(rnth1, [fail]) :-
+    rnth1(1, [], _).
+test(rnth1) :-
+    rnth1(1, [A], A).
+test(rnth1) :-
+    rnth1(2, [A,_], A).
+test(rnth1, [fail]) :-
+    rnth1(0, [_], _).
+test(rnth1, [fail]) :-
+    rnth1(3, [_,_], _).
+test(rnth1, all(I-A == [3-a,2-b,1-c])) :-
+    rnth1(I, [a,b,c], A).
+
+:- end_tests(problem_102).
+
