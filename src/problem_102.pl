32e30a8f29116a95d6e995e7df73806f814066f3
Vasily Kartashov <info@kartashov.com>
2022-08-13 19:38:31 +1000

    102

--- src/problem_102.pl
+++ src/problem_102.pl
@@ -1,27 +1,33 @@
-:- module(problem_102, [rnth1/3]).
+:- module(problem_102, [rnth0/3]).
 
-:- use_module(library(clpfd)).
 :- use_module(library(lists)).
 
-rnth1(I, A, E) :-
-    I #> 0,
-    length(A, L),
-    J #= L - I,
-    nth0(J, A, E).
+rnth0(N, Es, E) :-
+    (   integer(N) ->
+	rnth0_index(N, Es, E)
+    ;   rnth0_search(N, Es, E)
+    ).
+
+rnth0_index(N, Es, E) :-
+    length(Es, L),
+    I is L - N - 1,
+    nth0(I, Es, E).
+
+rnth0_search(N, Es, E) :-
+    reverse(Es, Es1),
+    nth0(N, Es1, E).
 
 :- begin_tests(problem_102).
 
-test(rnth1, [fail]) :-
-    rnth1(1, [], _).
-test(rnth1) :-
-    rnth1(1, [A], A).
-test(rnth1) :-
-    rnth1(2, [A,_], A).
-test(rnth1, [fail]) :-
-    rnth1(0, [_], _).
-test(rnth1, [fail]) :-
-    rnth1(3, [_,_], _).
-test(rnth1, all(I-A == [3-a,2-b,1-c])) :-
-    rnth1(I, [a,b,c], A).
+test(rnth0, [fail]) :-
+    rnth0(0, [], _).
+test(rnth0) :-
+    rnth0(0, [A], A).
+test(rnth0) :-
+    rnth0(1, [A,_], A).
+test(rnth0, [fail]) :-
+    rnth0(2, [_,_], _).
+test(rnth0, all(I-A == [0-c,1-b,2-a])) :-
+    rnth0(I, [a,b,c], A).
 
 :- end_tests(problem_102).
