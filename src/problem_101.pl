c49c5499ab3645e6f003ca3ef79526accded93c5
Vasily Kartashov <info@kartashov.com>
2022-08-14 09:36:33 +1000

    101

--- src/problem_101.pl
+++ src/problem_101.pl
@@ -1,17 +1,19 @@
 :- module(problem_101, [my_last/2]).
 
-my_last([A], A).
-my_last([_|T], X) :-
-    my_last(T, X).
+my_last([A|T], E) :-
+    my_last(T, A, E). 
+
+my_last([], E, E).
+my_last([A|T], _, E) :-
+    my_last(T, A, E).
 
 :- begin_tests(problem_101).
 
-test(my_last, [nondet]) :-
+test(my_last) :-
     my_last([A], A).
 test(my_last, [fail]) :-
     my_last([], _).
-test(my_last, [nondet]) :-
-    my_last([_,B|T], L),
-    my_last([B|T], L).
+test(my_last) :-
+    my_last([_,A], A).
 
 :- end_tests(problem_101).
