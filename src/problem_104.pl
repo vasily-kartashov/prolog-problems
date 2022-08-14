19c2e11939a24e7e11490911dd38d10644b483b4
Vasily Kartashov <info@kartashov.com>
2022-08-14 17:42:18 +1000

    104 114

--- src/problem_104.pl
+++ src/problem_104.pl
@@ -1,13 +1,26 @@
-:- module(problem_104, [length/2]).
+:- module(problem_104, [my_length/2]).
 
-:- use_module(library(clpz)).
+:- use_module(library(clpfd)).
 
-length(L, N) :-
-    #N #>= 0,
-    length(L, 0, N).
+my_length(L, N) :-
+    N #>= 0,
+    my_length(L, 0, N).
 
-length([], N, N).
-length([_|T], I, N) :-
+my_length([], N, N).
+my_length([_|T], I, N) :-
     I #< N,
     I1 #= I + 1,
-    length(T, I1, N).
+    my_length(T, I1, N).
+
+:- begin_tests(problem_104).
+
+test(my_length) :-
+    my_length([], 0).
+test(my_length) :-
+    my_length(A, 0),
+    A == [].
+test(my_length) :-
+    my_length([], N),
+    N =:= 0.
+
+:- end_tests(problem_104).
--- src/problem_114.pl
+++ src/problem_114.pl
@@ -3,3 +3,16 @@
 dupli([], []).
 dupli([A|T], [A,A|S]) :-
     dupli(T, S).
+
+:- begin_tests(problem_114).
+
+test(dupli) :-
+    dupli([], []).
+test(dupli) :-
+    dupli([A], [A,A]).
+test(dupli) :-
+    dupli(A, [a,a,b,E]),
+    A == [a, b],
+    E == b.
+    
+:- end_tests(problem_114).
